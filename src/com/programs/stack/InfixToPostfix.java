785a0794e8b0cc10be3e7eff8fb1fb1065e95d6d
Priyanka Sarkar <Priyanka_Sarkar@epam.com>
2019-04-13 11:35:21 +0530

    Conversion of Infix expression to Postfix expression

--- src/com/programs/stack/InfixToPostfix.java
+++ src/com/programs/stack/InfixToPostfix.java
@@ -13,7 +13,12 @@ import java.util.Stack;
 public class InfixToPostfix {
 
   private static final Map<Character, Integer> precedence = new HashMap<>();
-  
+
+  /*
+   * Initializes the static final field "precedence" according to the precedence
+   * of the different operators.
+   */
+
   static {
     precedence.put('+', 1);
     precedence.put('-', 1);
@@ -21,55 +26,83 @@ public class InfixToPostfix {
     precedence.put('/', 3);
     precedence.put('^', 4);
   }
-  
+
   private InfixToPostfix() {
-    
+    // private constructor to ensure non-instantiability. (Utility class)
   }
-  
+
   public static String convertToPostfix(String infix) {
-    
+
     StringBuilder postfix = new StringBuilder();
-    
+
     Stack<Character> stack = new Stack<>();
-    
-    for(int i = 0; i < infix.length(); i++) {
-      
+
+    for (int i = 0; i < infix.length(); i++) {
+
       char scannedCharacter = infix.charAt(i);
-      
-      if(scannedCharacter == '(') {
+
+      /*
+       * If the scanned character is a '(', push to stack.
+       */
+
+      if (scannedCharacter == '(') {
         stack.push(scannedCharacter);
       }
-      
-      else if(scannedCharacter == ')') {
-        while(stack.peek() != '(') {
+
+      /*
+       * If the scanned character is ')' pop and append to result string until '(' is
+       * found in the stack. Then finally pop '(' also from the stack.
+       */
+
+      else if (scannedCharacter == ')') {
+        while (stack.peek() != '(') {
           postfix.append(stack.pop());
         }
         stack.pop();
       }
-      
-      else if(!isOperator(scannedCharacter)) {
+
+      /*
+       * If the scanned character is an operand, append to result string.
+       */
+
+      else if (!isOperator(scannedCharacter)) {
         postfix.append(scannedCharacter);
       }
-      
+
+      /*
+       * If the scanned character is an operator, push to the stack only if any of the
+       * following conditions hold true :
+       *
+       * 1. If the stack is empty.
+       * 2. If the character on top of the stack if a '('
+       *    character.
+       * 3. If the scanned character is of higher precedence than the
+       * character on top of the stack.
+       * 
+       * Pop from the stack and append to the result string otherwise.
+       * 
+       */
+
       else {
-        while(!stack.isEmpty() && stack.peek() != '(' && precedence.get(scannedCharacter) <= precedence.get(stack.peek())) {
+        while (!stack.isEmpty() && stack.peek() != '('
+            && precedence.get(scannedCharacter) <= precedence.get(stack.peek())) {
           postfix.append(stack.pop());
         }
         stack.push(scannedCharacter);
       }
-      
+
     }
-    
-    while(!stack.isEmpty()) {
+
+    while (!stack.isEmpty()) {
       postfix.append(stack.pop());
     }
-    
+
     return postfix.toString();
-    
+
   }
 
   private static boolean isOperator(char ch) {
     return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^';
   }
-  
+
 }
