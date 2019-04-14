b56140462b03fd131c1f622b062e12ca67a0b125
coderPriyanka <4piyasarkar@gmail.com>
2019-04-14 18:55:59 +0530

    Conversion of Prefix expression to Infix expression.

--- src/com/programs/stack/PrefixToInfix.java
+++ src/com/programs/stack/PrefixToInfix.java
@@ -4,22 +4,24 @@ import java.util.Stack;
 
 public class PrefixToInfix {
   
-  public static String convertToInfix(String prefix) {
-    Stack<String> stack = new Stack<>();
-    for(int i = prefix.length() - 1; i >= 0; i--) {
-      char scannedCharacter = prefix.charAt(i);
-      if(isOperator(scannedCharacter)) {
-        String operand1 = stack.pop();
-        String operand2 = stack.pop();
-        stack.push("(" + operand1 + scannedCharacter + operand2 + ")");
-      }
-      else {
-        stack.push(scannedCharacter + "");
-      }
-    }
-    return stack.pop().toString();
-  }
-  private static boolean isOperator(char ch) {
-    return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^';
-  }
+	public static String convertToInfix(String prefix) {
+		Stack<String> stack = new Stack<>();
+		for(int i = prefix.length() - 1; i >= 0; i--) {
+			char scannerCharacter = prefix.charAt(i);
+			if(isOperator(scannerCharacter)) {
+				String operand1 = stack.pop();
+				String operand2 = stack.pop();
+				stack.push("(" + operand1 + scannerCharacter + operand2 + ")"); 
+			}
+			else {
+				stack.push(scannerCharacter + "");
+			}
+		}
+		return stack.pop();
+	}
+
+	private static boolean isOperator(char value) {
+		return value == '+' || value == '-' || value == '*' || value == '/' || value == '^';
+	}
+	
 }
